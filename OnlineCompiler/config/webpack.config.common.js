9dfa8c0e4154a5cee1e344f8d7ad301e0430335f
wojciechmizera <wizir@cyberdude.com>
2020-02-02 23:11:18 +0100

    Added typescript and react packages along with webpack-merge

--- OnlineCompiler/config/webpack.config.common.js
+++ OnlineCompiler/config/webpack.config.common.js
@@ -9,10 +9,18 @@ module.exports = {
         'Styles': './Style/Styles.scss' 
     },
     resolve: {
-        extensions: [ '.js', '.scss' ]
+        extensions: [ '.js', '.jsx', '.ts', 'tsx', '.scss' ]
     },
     plugins: [
         new AssetsPlugin({path: path.resolve(__dirname, '../wwwroot')})
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                use: 'ts-loader'
+            }
+        ]
+    }
     
 };
