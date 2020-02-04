b727753c794d5237ad0c093bdc88e6a925383e38
wojciechmizera <wizir@cyberdude.com>
2020-02-04 23:03:31 +0100

    Added reinfirced fluent configuration method (not working)

---
+++ OnlineCompiler/Utilities/ReinforcedTypingsConfiguration.cs
@@ -0,0 +1,15 @@
+using System;
+using Reinforced.Typings.Fluent;
+
+namespace OnlineCompiler.Utilities
+{
+    public static class ReinforcedTypingsConfiguration
+    {
+        public static void Configure(ConfigurationBuilder configBuilder)
+        {
+
+            int kk = 10;
+            throw new Exception();
+        }
+    }
+}
