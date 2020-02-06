04df156f4001747e85bba3444cec537a0b60a774
WojciechMizera <wojciech.mizera@fortedigital.no>
2020-02-06 17:30:54 +0100

    Locked Reinforced.Typings package untill it supports .net core 3.1.1

---
+++ OnlineCompiler/Api/LanguagesApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCompiler.Model;
+
+namespace OnlineCompiler.Api
+{
+    public class LanguagesApiController : Controller
+    {
+
+        [Route("/api/languages")]
+        public IEnumerable<ProgrammingLanguage> GetAvailableProgrammingLanguages()
+        {
+            return new[] {new ProgrammingLanguage()};
+        }
+    }
+}
--- OnlineCompiler/Model/ProgrammingLanguage.cs
+++ OnlineCompiler/Model/ProgrammingLanguage.cs
@@ -2,7 +2,7 @@ using Reinforced.Typings.Attributes;
 
 namespace OnlineCompiler.Model
 {
-    [TsInterface]
+    // [TsInterface] removed untill Reinforced.Typings supports .net core 3.1.1
     public class ProgrammingLanguage
     {
         public int Id { get; set; }
