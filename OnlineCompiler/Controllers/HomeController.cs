d6eac733926f583508557881910289a9bdc786bd
wojciechmizera <wizir@cyberdude.com>
2020-02-06 23:16:27 +0100

    Implemented LanguagesProvider.cs

--- OnlineCompiler/Controllers/HomeController.cs
+++ OnlineCompiler/Controllers/HomeController.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using OnlineCompiler.Features;
-using OnlineCompiler.Utilities;
 
 namespace OnlineCompiler.Controllers
 {
     public class HomeController : BaseController
     {
-        public HomeController(ILanguagesProvider provider)
-        {
-            var langs = provider.GetAvailable();
-        }
         
         public IActionResult Index()
         {
--- OnlineCompiler/Features/LanguagesProvider.cs
+++ OnlineCompiler/Features/LanguagesProvider.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Newtonsoft.Json;
 using OnlineCompiler.Model;
 
 namespace OnlineCompiler.Features
@@ -21,10 +25,31 @@ namespace OnlineCompiler.Features
         
         public IEnumerable<Language> GetAvailable()
         {
-            var path = _env.ContentRootPath;
-            
-            
-            return null;
+            foreach (var languageDir in _env.WebRootFileProvider
+                .GetDirectoryContents("lang_desc")
+                .Where(d => d.IsDirectory))
+            {
+                var files = Directory.GetFiles(languageDir.PhysicalPath).ToList();
+
+                var descriptorFile = files.Single(f => f.EndsWith("descriptor.json"));
+                var programFile = files.Single(f => f.EndsWith("descriptor.json") == false);
+                
+                var language = JsonConvert.DeserializeObject<Language>(ReadFile(descriptorFile));
+
+                language.DefaultProgram = ReadFile(programFile);
+                language.FileName = Path.GetFileName(programFile);
+
+                yield return language;
+            }
+        }
+
+        private static string ReadFile(string path)
+        {
+            using var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            using var reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+
         }
     }
 }
