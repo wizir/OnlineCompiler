78e51598bf8179778129c54af76a937046dbf9ad
wojciechmizera <wizir@cyberdude.com>
2020-02-11 21:59:06 +0100

    EditorComponent.tsx to class based component

--- OnlineCompiler/Components/Editor/EditorComponent.tsx
+++ OnlineCompiler/Components/Editor/EditorComponent.tsx
@@ -4,10 +4,26 @@ import * as React from "react";
 type EditorProps = {
 };
 
-const EditorComponent = (props: EditorProps) => {
+type EditorState = {
+    tabWidth: number,
+    indentation: number
+};
+
+
+class EditorComponent extends React.Component<EditorProps, EditorState> {
+    
+    constructor(props: EditorProps) {
+        super(props);
+
+        this.state = {
+            tabWidth: 4,
+            indentation: 0,
+            
+        };
+    }
     
     
-    const keyDownHandler = (e: KeyboardEvent): void => {
+    keyDownHandler = (e: KeyboardEvent): void => {
         if(e.key == 'Tab'){
             const target = e.currentTarget as HTMLTextAreaElement;
             const index = target.selectionStart;
@@ -15,26 +31,29 @@ const EditorComponent = (props: EditorProps) => {
                 .substr(0, target.selectionStart)
                 .split(/\r?\n|\r/);
             
-            // TODO might want to save these line and column number and show them somewhere, also tabulation count might should be editable
+            // TODO might want to save these line and column number and show them somewhere
             const lineNo = lines.length;
             const colNo =  lines[lines.length -1].length;
-            const count = 4 - colNo % 4;
+            const count = this.state.tabWidth - colNo % this.state.tabWidth;
             target.value = target.value.slice(0, index) + ' '.repeat(count) + target.value.slice(index);
             target.selectionStart = target.selectionEnd = index + count;
             e.preventDefault();
         }
     };
     
-    return (
-      <div className="tile">
-          <div className="Editor_Content">
-              <h3>Editor</h3>9
-              <div className="Editor_TextareaContainer">
-                  <textarea className="Editor_Textarea" onKeyDown={keyDownHandler.bind(this)} onBlur={() => {}}/>
-              </div>
-          </div>
-      </div>
-    )
-};
+    render() {
+        return (
+            <div className="tile">
+                <div className="Editor_Content">
+                    <h3>Editor</h3>9
+                    <div className="Editor_TextareaContainer">
+                        <textarea className="Editor_Textarea" onKeyDown={this.keyDownHandler.bind(this)} onBlur={() => {
+                        }}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default  EditorComponent;
